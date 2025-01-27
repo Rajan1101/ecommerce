976b0b1cf8b568cbe1148142f89c5646aa099c9a
Rajan1101 <rajansoni948148@gmai.com>
2025-01-27 22:42:53 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { LeftSideBarComponent } from './left-side-bar/left-side-bar.component';
+import { RightSideBarComponent } from './right-side-bar/right-side-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './header/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    LeftSideBarComponent,
+    RightSideBarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    CommonModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
